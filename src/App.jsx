f04b6faa7c844da245a2f15aa79e93c2cb4e390e
zoonnee <zooeunlee@gmail.com>
2026-03-13 18:18:47 +0900

    Update: update to hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import PortfolioGeneral from './PortfolioGeneral';
 import PortfolioDerma from './PortfolioDerma';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         {/* 기존 링크 대응: 원래 주소(/)로 오면 365용 노출 */}
         <Route path="/" element={<PortfolioGeneral />} /> 
@@ -12,7 +12,7 @@ function App() {
         {/* 새로운 링크: /centum으로 오면 센텀용 노출 */}
         <Route path="/derma" element={<PortfolioDerma />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
